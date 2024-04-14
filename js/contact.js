bf2a0c0ab61c3e879dd1d2535a591a6c6f5b2597
Robsunnn <robertospasors@gmail.com>
2024-04-14 14:31:10 +0300

    testing send email service with javascript

---
+++ js/contact.js
@@ -0,0 +1,33 @@
+const sendButton = document.getElementById('send-btn');
+
+sendButton.addEventListener('click', sendMail);
+
+
+
+function sendMail() {
+    const nameField = document.getElementById('name');
+    const emailField = document.getElementById('email');
+    const messageField = document.getElementById('message');
+
+    if (nameField.value === '' || emailField.value === '' || messageField.value === '') return;
+
+    let params = {
+        name: nameField.value,
+        email: emailField.value,
+        message: messageField.value,
+    };
+
+    const serviceID = 'service_3m9okqc';
+    const templateID = 'template_gexa6zi';
+
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            (res) => {
+                // name.value = '';
+                // emailField.value = '';
+                // messageField.value = '';
+                console.log(res)
+                alert('your message is sent successfully')
+            }
+        ).catch(err => console.log(err))
+}
