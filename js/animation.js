5182ce142570887062f6852a7b87181097ee8857
Robsunnn <robertospasors@gmail.com>
2024-09-10 18:07:57 +0300

    Added more styles and animations.

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const elementsToAnimate = [
         { element: document.querySelector('.info-about-me'), class: 'in-view' },
+
     ];
 
     function checkInView() {
+        console.log(elementsToAnimate);
         elementsToAnimate.forEach(item => {
             const rect = item.element.getBoundingClientRect();
             if (rect.top < window.innerHeight && rect.bottom > 0) {
@@ -15,5 +17,4 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener('scroll', checkInView);
     checkInView();
 });
-
 
