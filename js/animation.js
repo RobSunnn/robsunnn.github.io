6ce2fd46f8e8aea530a291b890785627a2b73877
Robsunnn <robertospasors@gmail.com>
2024-09-10 14:48:08 +0300

    Testing new design of the page...

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const elementsToAnimate = [
         { element: document.querySelector('.info-about-me'), class: 'in-view' },
-        { element: document.querySelector('.carousel-container'), class: 'in-view' },
-        { element: document.querySelector('.contact-form'), class: 'in-view'},
-        { element: document.querySelector('.quotes'), class: 'in-view'}
     ];
 
     function checkInView() {
@@ -16,36 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     window.addEventListener('scroll', checkInView);
-    checkInView(); // Check initially in case any element is already in view
+    checkInView();
 });
 
-const educationBtn = document.getElementById("education-btn");
-const contactBtn = document.getElementById("contact-btn");
-const homeBtn = document.getElementById("home-btn");
 
-const btnArr = [educationBtn, contactBtn, homeBtn];
-
-btnArr.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-        event.preventDefault();
-        if (btn.id === "education-btn") {
-            document.getElementById("carouselExampleIndicators").scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-                inline: "nearest"
-            });
-        } else if (btn.id === "contact-btn") {
-            document.getElementById("contact").scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-                inline: "nearest"
-            });
-        } else {
-            document.getElementById("name-and-title").scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-                inline: "nearest"
-            });
-        }
-    })
-})
