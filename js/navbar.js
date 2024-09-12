f54cfa2725960659a5ab350c56834f36eb8f3139
Robsunnn <robertospasors@gmail.com>
2024-09-12 11:20:45 +0300

    Added some styles

--- js/navbar.js
+++ js/navbar.js
@@ -1,28 +1,28 @@
-let prevScrollPos = window.scrollY;
-
-function collapseNavbar() {
-    let navbarToggle = document.querySelector('.navbar-toggler');
-    let navbarCollapse = document.querySelector('.navbar-collapse');
-
-    if (navbarCollapse.classList.contains('show')) {
-        navbarToggle.click(); // Programmatically click the toggle to collapse the navbar
-    }
-}
-
-document.addEventListener('click', function (event) {
-    let clickInside = document.getElementById('navbar').contains(event.target);
-    if (!clickInside) {
-        collapseNavbar();
-    }
-});
-
-window.addEventListener('scroll', function () {
-    let currentScrollPos = window.scrollY;
-    if (prevScrollPos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        collapseNavbar();
-        document.getElementById("navbar").style.top = "-400px";
-    }
-    prevScrollPos = currentScrollPos;
-});
+// let prevScrollPos = window.scrollY;
+//
+// function collapseNavbar() {
+//     let navbarToggle = document.querySelector('.navbar-toggler');
+//     let navbarCollapse = document.querySelector('.navbar-collapse');
+//
+//     if (navbarCollapse.classList.contains('show')) {
+//         navbarToggle.click(); // Programmatically click the toggle to collapse the navbar
+//     }
+// }
+//
+// document.addEventListener('click', function (event) {
+//     let clickInside = document.getElementById('navbar').contains(event.target);
+//     if (!clickInside) {
+//         collapseNavbar();
+//     }
+// });
+//
+// window.addEventListener('scroll', function () {
+//     let currentScrollPos = window.scrollY;
+//     if (prevScrollPos > currentScrollPos) {
+//         document.getElementById("navbar").style.top = "0";
+//     } else {
+//         collapseNavbar();
+//         document.getElementById("navbar").style.top = "-400px";
+//     }
+//     prevScrollPos = currentScrollPos;
+// });
