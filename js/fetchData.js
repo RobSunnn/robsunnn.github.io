1d6efb71edd69ae22dcf87afcfcede6ff7020a91
Robsunnn <robertospasors@gmail.com>
2024-09-24 11:41:36 +0300

    Fixing the problem with multi word cities

--- js/fetchData.js
+++ js/fetchData.js
@@ -81,18 +81,17 @@ export async function fetchFact() {
 }
 
 export async function getWeatherInfo(city) {
+    const url = `https://robsunnn-api.azurewebsites.net/weather`;
     if (city.length > 50) {
         throw new Error('Please enter a valid city name');
     }
-    const encodedCity = encodeURIComponent(city.trim());
-    const url = `https://robsunnn-api.azurewebsites.net/weather?city=${encodedCity}`;
-
+    const formattedCity = city.replace(/\s+/g, '%20');
     const response = await fetch(url, {
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        mode: 'cors'
+        body: JSON.stringify({ city: formattedCity })  // Send the city as part of the request body
     });
 
     if (!response.ok) {
