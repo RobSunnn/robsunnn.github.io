6132a1a1b805ddaa8b2f77f96d5b39ebecf585e7
Rob$rt0 <RobertoSpasovrs@gmail.com>
2024-12-15 17:58:54 +0200

    Added new element in education

--- js/education.js
+++ js/education.js
@@ -131,7 +131,7 @@ function generateCarousel() {
 
         const cardText = document.createElement('p');
         cardText.classList.add('carousel-card-text');
-        if (index === 0) {
+        if (index === 1) {
             cardText.innerHTML = `${slide.pText}<br><br> This is the repository holding my project defence for this course: <a href="${"https://github.com/robsunnn/hotelapp"}" target="_blank">Hotel MS</a> <br> <br> The app is deployed here: <a href="${"https://hotel-ms.azurewebsites.net/"}" target="_blank">Hotel MS</a>`
         } else {
             cardText.textContent = slide.pText;
@@ -189,7 +189,12 @@ function createCloseButton() {
 
 function getSlides() {
     return [
-
+        {
+           name: 'Software Engineer',
+           anchor: 'https://softuni.bg/certificates/details/230308/45695904',
+           pText: '',
+           imgSource: 'images/certificates/myDiploma.png'
+        },
         {
             name: 'Spring Advanced',
             anchor: 'https://softuni.bg/certificates/details/223730/0fcad70e',
